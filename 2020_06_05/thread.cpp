10aad7b25c6367e4db03d5e56d4dba2751e1079a
CodeHina <1398869163@qq.com>
2020-06-05 05:45:24 -0700

    增加线程创建失败的错误打印,学习strerror_r()函数

--- 2020_06_05/thread.cpp
+++ 2020_06_05/thread.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <unistd.h>
 #include <pthread.h>
-
+#include <cstring>
 
 void* thrd_func(void* arg)//子线程主控函数
 {
@@ -19,9 +19,15 @@ int main()
 
     pthread_t thrdid;//线程id
 
-    if( pthread_create(&thrdid, NULL, thrd_func, NULL) != 0) //成功返回0/失败返回errno
+    //int pthread_create(pthread_t* thread, const pthread_attr_t* attr, void* (*start_routine)(void*), void* arg);
+    //参数1:线程id 参数2:线程属性(默认属性传NULL) 参数3:线程主控函数 参数4:线程主控函数的参数
+    int ret = pthread_create(&thrdid, NULL, thrd_func, NULL); //成功返回0/失败返回errno
+    if( ret != 0) 
     {
-        std::cerr << "pthread_create error" << std::endl;
+        //char* strerror(int errnum) / char* strerror_r(int errnum, char* buf, size_t buflen) - 线程安全的
+        char buf[128];
+        strerror_r(ret, buf, sizeof(buf));
+        std::cerr << "pthread_create error : " << buf << std::endl;
         exit(-1);
     }
 
